0eb9b52af45583931466efb1a2c1b18352077ff0
Tri <triphan921@gmail.com>
2021-03-30 09:30:16 -0700

    render players on playersList

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Player = ({ player }) => {
+const Player = (props) => {
+  console.log(props)
   return (
     <div>
-      <h1>Name: {player.name}</h1>
-      
+      <h1>Name: {props.player.name}</h1>
+      <img className='card-image' src={props.player.img} alt='player' />
+      <h4>Height: {props.player.height}</h4>
+      <h4>Position: {props.player.position}</h4>
     </div>
   );
 }
