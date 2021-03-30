77c3795719321e45038c7f5e3c63aa3dceb600a2
Tri <triphan921@gmail.com>
2021-03-30 16:45:33 -0700

    add player and render team players

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,30 +1,15 @@
 import React from "react";
-// import Player from "./Player";
+import Player from "./Player";
 
-// const API2 = "http://localhost:3000/team";
-
-class Team extends React.Component {
-  state = {
-    team: [],
-  };
-
-  // componentDidMount() {
-  //   fetch(API2)
-  //     .then((r) => r.json())
-  //     .then((team) => this.setState({ team }));
-  // }
-
-  render() {
-  
-    return (
-      <div>
-        <h1>Team</h1>
-        {/* {this.state.team.map((player) => (
-          <Player key={player.id} player={player} />
-        ))} */}
-      </div>
-    );
-  }
-}
+const Team = (props) => {
+  return (
+    <div>
+      <h1>Team</h1>
+      {props.team.map((player) => (
+        <Player key={player.id} player={player} />
+      ))}
+    </div>
+  );
+};
 
 export default Team;
