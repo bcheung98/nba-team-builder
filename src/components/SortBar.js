81e04248ebbb96be735bfd39999978b0926d5a2b
Brian <briantcheung@gmail.com>
2021-03-31 10:04:59 -0700

    Added custom sort function

---
+++ src/components/SortBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SortBar = (props) => {
+    return (
+        <React.Fragment>
+            <h4 style={{margin: "5px"}}>Sort by:</h4>
+            <select onChange={props.onChange}>
+                <option value="name-AZ">Name (A-Z)</option>
+                <option value="name-ZA">Name (Z-A)</option>
+                <option value="height-asc">Height (Ascending)</option>
+                <option value="height-desc">Height (Descending)</option>
+                <option value="weight-asc">Weight (Ascending)</option>
+                <option value="weight-desc">Weight (Descnding)</option>
+            </select>
+        </React.Fragment>
+    )
+}
+
+export default SortBar;
