6983e7811c3d238b8ddc80a46fce40447b433991
Brian <briantcheung@gmail.com>
2021-03-31 17:21:06 -0700

    Card formatting

--- src/components/DraftedPlayer.js
+++ src/components/DraftedPlayer.js
@@ -2,11 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import teamMap from "./TeamMap";
 
-const formatName = (name) => {
-  name = name.split(" ");
-  return `${name[0]}  ${name[1].toUpperCase()}`;
-};
-
 const formatHeight = (height) => {
   height = height.split("-");
   return `${height[0]} ft ${height[1]} in`;
@@ -21,7 +16,7 @@ const DraftedPlayer = (props) => {
           src={teamMap[props.player.team].logo}
           alt={props.player.team}
         />
-        <h3 className="player-name-card">{formatName(props.player.name)}</h3>
+        <h3 className="player-name-card">{props.player.name}</h3>
         <img
           className="player-img-card"
           src={props.player.img}
@@ -31,7 +26,7 @@ const DraftedPlayer = (props) => {
           <p><b>Height:</b> {formatHeight(props.player.height)}</p>
           <p><b>Weight:</b> {props.player.weight} lb</p>
           <p><b>Position</b>: {props.player.position}</p>
-          <Link to={`/players/${props.player.id}`}>More Info</Link>
+          <Link to={`/players/${props.player.id}`} style={{color: "white", fontWeight: "bold"}}>More Info</Link>
           <br />
           <button onClick={() => props.removePlayer(props.player)} className='button'>Undraft Player</button>
         </div>
